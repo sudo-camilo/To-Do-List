de1e214b0c6f8fc27f48ccb03b8ab8e7e28a7134
Tyler Truman <72750217+sudo-camilo@users.noreply.github.com>
2020-11-19 16:38:44 -0500

    Add files via upload

---
+++ To Do List/CIT265_Truman_T_A5/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CIT265_Truman_T_A5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.quit_button = new System.Windows.Forms.Button();
+            this.add_activity = new System.Windows.Forms.Button();
+            this.remove_activity = new System.Windows.Forms.Button();
+            this.sort_name = new System.Windows.Forms.Button();
+            this.date_sort = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(288, 55);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2020, 11, 12, 19, 2, 35, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // quit_button
+            // 
+            this.quit_button.Location = new System.Drawing.Point(342, 384);
+            this.quit_button.Name = "quit_button";
+            this.quit_button.Size = new System.Drawing.Size(93, 23);
+            this.quit_button.TabIndex = 7;
+            this.quit_button.Text = "&Quit";
+            this.quit_button.UseVisualStyleBackColor = true;
+            this.quit_button.Click += new System.EventHandler(this.quit_button_Click);
+            // 
+            // add_activity
+            // 
+            this.add_activity.Location = new System.Drawing.Point(288, 81);
+            this.add_activity.Name = "add_activity";
+            this.add_activity.Size = new System.Drawing.Size(91, 23);
+            this.add_activity.TabIndex = 1;
+            this.add_activity.Text = "&Add an Activity";
+            this.add_activity.UseVisualStyleBackColor = true;
+            this.add_activity.Click += new System.EventHandler(this.add_activity_Click);
+            // 
+            // remove_activity
+            // 
+            this.remove_activity.Location = new System.Drawing.Point(380, 81);
+            this.remove_activity.Name = "remove_activity";
+            this.remove_activity.Size = new System.Drawing.Size(108, 23);
+            this.remove_activity.TabIndex = 2;
+            this.remove_activity.Text = "&Remove an Activity";
+            this.remove_activity.UseVisualStyleBackColor = true;
+            this.remove_activity.Click += new System.EventHandler(this.remove_activity_Click);
+            // 
+            // sort_name
+            // 
+            this.sort_name.Location = new System.Drawing.Point(288, 355);
+            this.sort_name.Name = "sort_name";
+            this.sort_name.Size = new System.Drawing.Size(93, 23);
+            this.sort_name.TabIndex = 5;
+            this.sort_name.Text = "Sort by &Name";
+            this.sort_name.UseVisualStyleBackColor = true;
+            this.sort_name.Click += new System.EventHandler(this.sort_name_Click);
+            // 
+            // date_sort
+            // 
+            this.date_sort.Location = new System.Drawing.Point(397, 355);
+            this.date_sort.Name = "date_sort";
+            this.date_sort.Size = new System.Drawing.Size(91, 23);
+            this.date_sort.TabIndex = 6;
+            this.date_sort.Text = "Sort by &Date";
+            this.date_sort.UseVisualStyleBackColor = true;
+            this.date_sort.Click += new System.EventHandler(this.date_sort_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(288, 228);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 121);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 97);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(317, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Insert Event Description Here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.date_sort);
+            this.Controls.Add(this.sort_name);
+            this.Controls.Add(this.remove_activity);
+            this.Controls.Add(this.add_activity);
+            this.Controls.Add(this.quit_button);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button quit_button;
+        private System.Windows.Forms.Button add_activity;
+        private System.Windows.Forms.Button remove_activity;
+        private System.Windows.Forms.Button sort_name;
+        private System.Windows.Forms.Button date_sort;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ To Do List/CIT265_Truman_T_A5/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIT265_Truman_T_A5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void quit_button_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Would you like to quit the program?", "", MessageBoxButtons.YesNo); //gets the result after clicking quit
+            if (dialog == DialogResult.Yes)
+            {
+                TextWriter savedFile = new StreamWriter(@".\To-do List.txt"); //saving to file (relative)
+                foreach(var item in listBox1.Items)
+                {
+                    savedFile.WriteLine(item.ToString()); //writing to file
+                }
+                this.Close();
+                savedFile.Close();
+            }
+        }
+
+        private void add_activity_Click(object sender, EventArgs e)
+        {
+            DateTime date = this.dateTimePicker1.Value; //sets 'date' to the user input
+            listBox1.Items.Add(textBox1.Text + " " + date.ToString("MM/dd/yyyy")); //adding to the listbox
+            //clear the controls
+            dateTimePicker1.Value = DateTime.Today;
+            textBox1.Clear();
+        }
+
+        private void remove_activity_Click(object sender, EventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Would you like to remove the event?", "", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void sort_name_Click(object sender, EventArgs e)
+        {
+            List<string> nameSortArray = listBox1.Items.OfType<string>().ToList(); //casting to list
+            var sorted = from name in nameSortArray orderby name select name; //instantiate query
+            listBox1.Items.Clear(); //clear the listbox for new information
+            foreach(var item in sorted)
+            {
+                listBox1.Items.Add(item); //add the new stuff
+            }
+        }
+
+        private void date_sort_Click(object sender, EventArgs e)
+        {
+         
+            List<string> dateSortArray = listBox1.Items.OfType<string>().ToList(); //casting to list
+            var sortedYear = from name in dateSortArray orderby name.Substring(name.LastIndexOf("/")) select name; //instantiate query for year
+            var sortedNew = from name in sortedYear orderby name.Substring(name.LastIndexOf(" "), 5) select name; //instantiate query for mmdd
+            listBox1.Items.Clear(); //clear the listbox for new information
+            foreach (var item in sortedNew)
+            {
+                listBox1.Items.Add(item); //add the new stuff
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                StreamReader stream = new StreamReader(@".\To-do List.txt");
+                string text;
+                while ((text = stream.ReadLine()) != null) //making sure it does not add infinite blank lines
+                {
+                    listBox1.Items.Add(text);
+                }
+                stream.Close();
+            }
+            catch (FileNotFoundException message)
+            {
+                Console.WriteLine(message);
+            }
+            
+        }
+    }
+}
